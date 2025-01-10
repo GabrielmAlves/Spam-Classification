34342600cab3c0d1de74989bcf0f20aa32df8f4d
GabrielmAlves <gabrielmalves10@gmail.com>
2025-01-09 21:29:34 -0300

    finding spam.csv file on downloads folder

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+
+
+def join_directory(path1, path2):
+    
+    joined_path = os.path.join(path1, path2)
+    return joined_path
+
+
+def find_file(file):
+    
+    user_directory = os.path.expanduser("~")
+    downloads_folder = join_directory(user_directory, "Downloads")
+    file_path = join_directory(downloads_folder, file)
+    
+    if os.path.exists(file_path):
+        return file_path
+    else:
+        raise FileNotFoundError(f"File '{file}' was not found in '{downloads_folder}' folder..")
+
+def main():
+    try:
+        file = find_file("spam.csv")
+        print(f"File was found: {file}")
+    except FileNotFoundError as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
