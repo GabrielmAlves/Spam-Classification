42b652ef48192c55198e1861cb8ef7749f3a20da
GabrielmAlves <gabrielmalves10@gmail.com>
2025-01-16 23:13:44 -0300

    splitting sets into training, validation and test

--- src/data_split.py
+++ src/data_split.py
@@ -1,8 +1,15 @@
 from utils import (
     get_column
 )
+from sklearn.model_selection import train_test_split
 
 def sets_division(dataframe):
     labels = get_column(dataframe, 'v1')
     features = get_column(dataframe, 'v2')
     print(f"features: \n", features)
+    
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+    
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=42)
+    
+    return X_train, X_val, X_test, y_train, y_val, y_test
--- src/main.py
+++ src/main.py
@@ -14,7 +14,14 @@ def main():
         file = find_file("spam.csv")
         print(f"File was found: {file}")
         processed_file = process_text(file)
-        sets_division(processed_file)
+        X_train, X_val, X_test, y_train, y_val, y_test = sets_division(processed_file)
+        
+        print(f"\nX_train: {X_train}")
+        print(f"\n\nX_val: {X_val}")
+        print(f"\n\nX_test: {X_test}")
+        print(f"\n\ny_train: {y_train}")
+        print(f"\n\ny_val: {y_val}")
+        print(f"\n\ny_test: {y_test}")
     except FileNotFoundError as e:
         print(e)
 
